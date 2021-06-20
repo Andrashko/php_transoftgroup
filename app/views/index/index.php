c719c135d40bf0d95c0b9b0527726bea84832551
Anrashko <yurii.andrashko@uzhnu.edu.ua>
2021-06-20 15:10:06 +0300

    Task 10.2 login/logout

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -1,4 +1,15 @@
+<?php
+
+use Core\Helper;
+
+$customer = Helper::getCustomer();
+?>
+
+<?php if ($customer):?>
+<h3>Hello, <?php echo $customer['first_name']?></h3>
+<?php else: ?> 
 <h3>Hello, unauthorized user!</h3>
+<?php endif ?>
 <?php echo "hello"; ?>
 
 
